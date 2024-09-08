402faa7bae6be44d8a886e42953007854f185f55
Aleksey Kozlovskiy <lesha.kozlovskiy.2005@mail.ru>
2024-09-08 23:12:01 +0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import openpyxl
 from openpyxl.utils import column_index_from_string, get_column_letter
+import json
 
 def replace_words(replacements: dict, phrases: list):
+    print("Заменяем слова..")
     # Новый список для хранения результата
     result = []
 
@@ -18,6 +20,7 @@ def replace_words(replacements: dict, phrases: list):
                 # Если ключ найден в фразе, делаем замены и добавляем в результат
                 for value in values:
                     replased_phrase = phrase.replace(key, str(value))
+                    print(f"Заменяем '{phrase}' на '{replased_phrase}'")
                     result.append(replased_phrase)
                 
                 replaced = True
@@ -41,14 +44,19 @@ print("Читаем переменные..")
 # Читаем переменные
 variables_dict = {}
 for row in sheet_variables.iter_rows(values_only=True):
+    # print(F'Записываем в словарь "{row[0].value}" из листа variables')
+    # if row[0].value:
+        # print("row", row[0].value)
     var_name, *var_values = row
     var_values = [var for var in var_values if var]
     if var_values:
         variables_dict[var_name] = var_values
+print("variables_dict:", variables_dict)
 print("Чтение и обработка данных из листа script..")
 # Чтение и обработка данных из листа script
 for row in sheet_script.iter_rows(min_row=1, max_col=2):
     if row[0].value:
+        print(f"Чтение ячеек из листа script. Значение {row[0].value}. Ячейка: {row}")
         column_a = row[0].value.lower().strip() if row[0].value else None
         column_a = column_a.split("-")
 
@@ -61,6 +69,7 @@ for row in sheet_script.iter_rows(min_row=1, max_col=2):
                 pass
         result_array = []
         for col in selected_columns:
+            print(f"Работаем со столбоцом {get_column_letter(col)} в листе words")
             retry = 1
             multiplier = sheet_words.cell(row=3, column=col).value
             try:
@@ -70,15 +79,16 @@ for row in sheet_script.iter_rows(min_row=1, max_col=2):
             if not isinstance(multiplier, int) | isinstance(multiplier, float):
                 print(f"В столбце {get_column_letter(col)} в третьей строке на листе words не цифра, пропускаем столбец. Значение в ячейке: {multiplier}")
                 raise Exception
-            for cell in sheet_words.iter_rows(min_row=4, min_col=col, max_col=col, max_row=sheet_words.max_row, values_only=True):
-                word = cell[0]
+            for cell in sheet_words.iter_rows(min_row=4, min_col=col, max_col=col, max_row=sheet_words.max_row):
+                word = cell[0].value
                 if word:
+                    word = word.strip()
+                    print("Работаем над словом:", word, "Ячейка:", cell)
                     if ")-(" in word:
                         retry = len(word.split(")-("))
                     # Умножаем слово на значение из второй строки
                     words_multiplied = [word] * abs(multiplier)
                         
-                    # Обработка дефисов
                     for word_variant in words_multiplied:
                         result_array.append(word_variant)
             
@@ -87,21 +97,17 @@ for row in sheet_script.iter_rows(min_row=1, max_col=2):
         for i in range(retry):
             processed_array = replace_words(variables_dict, processed_array)
 
-
-        # Формируем массив в формате JSON
-        json_array = [f'"{word}"' for phrase  in processed_array for word in phrase.split(", ")]
-        json_array_2 = []
-        for word in json_array:
+        json_array = []
+        for word in processed_array:
+            word = word.replace('"', "")
             if "-" in word:
-                json_array_2.append(word)
-                json_array_2.append(word.replace("-", ""))
-                json_array_2.append(word.replace("-", " "))
+                json_array.append(word)
+                json_array.append(word.replace("-", ""))
+                json_array.append(word.replace("-", " "))
             else:
-                json_array_2.append(word.replace("(", "").replace(")", ""))
-        
-        json_result = "[" + ", ".join(json_array_2) + "]"
+                json_array.append(word.replace("(", "").replace(")", ""))
         # Записываем результат в колонку B
-        row[1].value = json_result
+        row[1].value = json.dumps(json_array, ensure_ascii=False)
 
 print("Сохраняем файл..")
 # Сохраняем изменения
